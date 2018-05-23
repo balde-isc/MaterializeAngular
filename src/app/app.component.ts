e5991bd224e795e0db155221650b7e133d059f7e
Baldemar Guzman <baldemar.isc@gmail.com>
2018-05-23 12:58:20 -0500

    Materialize

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import {
+  Component,
+  EventEmitter
+} from '@angular/core';
+import { MaterializeAction } from 'angular2-materialize';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  modalActions = new EventEmitter<MaterializeAction>();
+  openModal() {
+    this.modalActions.emit({ action: 'modal', params: ['open'] });
+  }
+  closeModal() {
+    this.modalActions.emit({ action: 'modal', params: ['close'] });
+  }
+}
